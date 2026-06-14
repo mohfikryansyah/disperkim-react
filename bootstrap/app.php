b316aa207fa3570deb0dffb07e82e9ffa1bf9291
mohfikryansyah <moh.fikryansyah@gmail.com>
2026-06-14 21:33:08 +0800

    fix: Correct error handling condition for production environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,7 +35,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
             /** Tambahkan "!" diawal sebelum App saat production */
-            if (App::environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if (! App::environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('error', [
                     'status' => $response->getStatusCode(),
                     'homeUrl' => $request->user() ? route('dashboard') : url('/'),
